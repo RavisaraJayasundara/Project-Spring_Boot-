6643641ec59292f7625e2a939e3ded50d4021be5
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-05-09 23:08:59 +0530

    @GetMapping for getting data from database

--- employeeapp/src/main/java/ac/lk/vau/fas/ict/controller/UserController.java
+++ employeeapp/src/main/java/ac/lk/vau/fas/ict/controller/UserController.java
@@ -1,7 +1,11 @@
 package ac.lk.vau.fas.ict.controller;
 
 import ac.lk.vau.fas.ict.model.User;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +21,9 @@ public class UserController {
      User newUser(@RequestBody User newUser) {
 		return userRepository.save(newUser);
     	 
+     }
+     @GetMapping("/users")
+     List<User> getAllUsers(){
+    	 return userRepository.findAll();
      }
 }
