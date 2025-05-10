4797190ec43937d49303edf490d04d667503f006
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-05-10 20:57:36 +0530

    create Navbar

---
+++ frontend/src/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav  className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">Full Stack Application</a>
+    
+        <button className="btn btn-outline-light ">
+                Add User
+        </button>
+      </nav>
+    </div>
+  );
+}
